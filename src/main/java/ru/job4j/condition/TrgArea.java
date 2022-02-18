7f7f66f7997fb367ece69bd8165350cc8c7d922a
Ash <ashot.93@mail.ru>
2022-02-19 01:23:44 +0300

    Площадь треугольника. [#175474]

---
+++ src/main/java/ru/job4j/condition/TrgArea.java
@@ -0,0 +1,13 @@
+package ru.job4j.condition;
+
+public class TrgArea {
+    public static double area(double a, double b, double c) {
+        double sum = a + b + c;
+        return Math.sqrt(sum);
+    }
+
+    public static void main(String[] args) {
+        double rsl = TrgArea.area(4, 4, 4);
+        System.out.println("area (4, 4, 4) = " + rsl);
+    }
+}
